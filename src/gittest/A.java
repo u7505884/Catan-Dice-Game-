1759702de5c107f186d38fe1f22d5151b53d5aa3
u7546144 <u7546144@anu.edu.au>
2022-08-24 12:51:06 +1000

    Added class Main and class A, toString

--- src/gittest/A.java
+++ src/gittest/A.java
@@ -1,7 +1,8 @@
 package gittest;
 
 public class A {
-    public static void main(String[] args) {
-        System.out.println("A");
+    @Override
+    public String toString() {
+        return "A";
     }
 }
