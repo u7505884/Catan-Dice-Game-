920678dd0b7a0277e09920c7215f20a536ffc088
u7544188 <u7544188@anu.edu.au>
2022-09-09 15:45:47 +1000

    task 5 : hexagon

--- src/comp1110/ass2/gui/Viewer.java
+++ src/comp1110/ass2/gui/Viewer.java
@@ -9,18 +9,20 @@ import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Polygon;
 import javafx.stage.Stage;
 
 public class Viewer extends Application {
 
     private static final int VIEWER_WIDTH = 1200;
     private static final int VIEWER_HEIGHT = 700;
+    private static final int HEX_HEIGHT = 150;
 
     private final Group root = new Group();
     private final Group controls = new Group();
     private TextField playerTextField;
     private TextField boardTextField;
-
 
     /**
      * Show the state of a (single player's) board in the window.
@@ -29,8 +31,81 @@ public class Viewer extends Application {
      */
     void displayState(String board_state) {
         // FIXME Task 5: implement the state viewer
+        this.makeBoard();
     }
+    private void makeBoard(){
+        Double side = (Double) (HEX_HEIGHT * Math.sqrt(3));
+        Polygon hex1 = new Polygon();
+        hex1.getPoints().addAll(-side / 2, -75.0,
+                -side, 0.0,
+                -side / 2, 75.0,
+                side / 2, 75.0,
+                side , 0.0,
+                side / 2, -75.0);
+        hex1.setLayoutX(600);
+        hex1.setLayoutY(200);
+        hex1.setFill(Color.GREEN);
+        Polygon hex2 = new Polygon();
+        hex2.getPoints().addAll(
+                -side/2, -75.0,
+                -side, 0.0,
+                -side/2, 75.0,
+                side/2, 75.0,
+                side, 0.0,
+                side/2, -75.0);
+        hex2.setLayoutX(600 - 3 * side / 2);
+        hex2.setLayoutY(425);
+        hex2.setFill(Color.RED);
+        Polygon hex3 = new Polygon();
+        hex3.getPoints().addAll(
+                -side/2, -75.0,
+                -side, 0.0,
+                -side/2, 75.0,
+                side/2, 75.0,
+                side, 0.0,
+                side/2, -75.0);
+        hex3.setLayoutX(600);
+        hex3.setLayoutY(500);
+        hex3.setFill(Color.YELLOW);
 
+        Polygon hex4 = new Polygon();
+        hex4.getPoints().addAll(
+                -side/2, -75.0,
+                -side, 0.0,
+                -side/2, 75.0,
+                side/2, 75.0,
+                side, 0.0,
+                side/2, -75.0);
+        hex4.setLayoutX(600 + 3 * side / 2);
+        hex4.setLayoutY(425);
+        hex4.setFill(Color.LIGHTGREY);
+
+        Polygon hex5 = new Polygon();
+        hex5.getPoints().addAll(
+                -side/2, -75.0,
+                -side, 0.0,
+                -side/2, 75.0,
+                side/2, 75.0,
+                side, 0.0,
+                side/2, -75.0);
+        hex5.setLayoutX(600 + 3 * side / 2);
+        hex5.setLayoutY(275);
+        hex5.setFill(Color.BLUE);
+
+        Polygon hex6 = new Polygon();
+        hex6.getPoints().addAll(
+                -side/2, -75.0,
+                -side, 0.0,
+                -side/2, 75.0,
+                side/2, 75.0,
+                side, 0.0,
+                side/2, -75.0);
+        hex6.setLayoutX(600 - 3 * side / 2);
+        hex6.setLayoutY(275);
+        hex6.setFill(Color.BLACK);
+
+        root.getChildren().addAll(hex6, hex1, hex2, hex3, hex4, hex5);
+    }
     /**
      * Create a basic text field for input and a refresh button.
      */
