7145b3d67577be4274f98401465653090bf22fa6
u7505884 <u7505884@anu.edu.au>
2022-09-26 02:12:39 +1000

    Completed BulidableStructuresTest!

--- src/comp1110/ass2/gui/Game.java
+++ src/comp1110/ass2/gui/Game.java
@@ -11,6 +11,7 @@ public class Game extends Application {
     private static final int WINDOW_WIDTH = 1200;
     private static final int WINDOW_HEIGHT = 700;
 
+
     @Override
     public void start(Stage stage) throws Exception {
         Scene scene = new Scene(this.root, WINDOW_WIDTH, WINDOW_HEIGHT);
---
+++ tests/comp1110/ass2/BuildableStructuresTest.java
@@ -0,0 +1,111 @@
+package comp1110.ass2;
+
+import comp1110.ass2.BuildableStructures;
+import org.junit.jupiter.api.Test;
+
+import java.util.Random;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BuildableStructuresTest {
+    private void setterTest(BuildableStructures b, boolean whetherBuild)
+    {
+        b.setWhetherBuild(whetherBuild);
+        assertEquals(whetherBuild,b.whetherHaveBuilt, "Setter method did not set value correctly.");
+    }
+
+    private void getterIndexTest(BuildableStructures b, int x) {
+        b.index = x;
+        assertEquals(x, b.getX(), "Getter method did not return value correctly.");
+    }
+    private void getterScoresTest(BuildableStructures b, int scores) {
+        b.scores = scores;
+        assertEquals(scores, b.getScores(), "Getter method did not return value correctly.");
+    }
+    private void getterDORTest(BuildableStructures b,  int[] demandOfResources) {
+        b.demandOfResources = demandOfResources;
+        assertEquals(demandOfResources, b.getDemandOfResources(), "Getter method did not return value correctly.");
+    }
+    private void getterLBSTest(BuildableStructures b, BuildableStructures lastBuildableStructure) {
+        b.lastBuildableStructure = lastBuildableStructure;
+        assertEquals(lastBuildableStructure, b.getLastBuildableStructure(), "Getter method did not return value correctly.");
+    }
+    private void getterWHBTest(BuildableStructures b,  boolean whetherHaveBuilt) {
+        b.whetherHaveBuilt = whetherHaveBuilt;
+        assertEquals(whetherHaveBuilt, b.getWhetherHaveBuilt(), "Getter method did not return value correctly.");
+
+    }
+
+    private void testResConstraint(BuildableStructures b, int[] currentResources, boolean expected) {
+        assertNotNull(currentResources,"Expected non-null object, but got null.");
+        boolean out = b.resourcesConstraint(currentResources);
+        assertEquals(expected, out, "Unexpected result for '" + currentResources+ "'");
+    }
+
+
+    @Test
+    public void GetterIndex() {
+        BuildableStructures b = new BuildableStructures();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            int v = r.nextInt(20);
+            getterIndexTest(b, v);
+        }
+    }
+
+    @Test
+    void getScores() {
+        BuildableStructures b = new BuildableStructures();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            int v = r.nextInt(100);
+            getterScoresTest(b, v);
+        }
+    }
+
+    @Test
+    void setgetWhetherBuild() {
+        BuildableStructures b = new BuildableStructures();
+        Random r = new Random();
+        for (int i = 0; i < 10; i++) {
+            boolean v = r.nextBoolean();
+            setterTest(b, v);
+            getterWHBTest(b,v);
+        }
+    }
+
+    @Test
+    void getDemandOfResources() {
+        BuildableStructures b = new BuildableStructures();
+        Random r = new Random();
+        int[] arr=new int[r.nextInt(20)];
+        for(int i=0;i<arr.length;i++)
+            arr[i]=(int)(Math.random());
+        for (int i = 0; i < 10; i++) {
+            int v = r.nextInt(100);
+            getterDORTest(b, arr);
+        }
+    }
+
+    @Test
+    void getLastBuildableStructure() {
+        GetterIndex() ;
+        getScores();
+        setgetWhetherBuild();
+        getDemandOfResources();
+        }
+
+
+    @Test
+    void resourcesConstraint() {
+        BuildableStructures b = new BuildableStructures();
+        int[] a=new int[]{1,1,1,0,0,0};
+        int[] c=new int[]{1,1,1,-2,3,0};
+        int[] d=new int[]{1,3,1,2,5,2};
+        int[] e=new int[]{3,2,0,-1,1,1};
+        testResConstraint(b,a,true);
+        testResConstraint(b,c,false);
+        testResConstraint(b,d,true);
+        testResConstraint(b,e,false);
+    }
+}
